320ad105188f22ee4047182d51301ac504af4bea
Alex Holachek <aholachek@gmail.com>
2020-01-30 22:18:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require('fs-extra')
+const inquirer = require('inquirer')
+const {
+  visualizeBundles,
+  initDownloadFilesFlow,
+  createListOfLocalPaths,
+  coverageAndDownloadPrompts,
+  jsURLsFromCoverage
+} = require('./helpers')
+
+inquirer.registerPrompt('fuzzypath', require('inquirer-fuzzy-path'))
+
+// https://developers.google.com/web/tools/chrome-devtools/coverage
+const visHTML = 'bundles.html'
+
+if (fs.existsSync(visHTML)) {
+  fs.unlinkSync(visHTML)
+}
+
+const bundleFolderName = 'sourcemap-wizard-downloads'
+
+const shouldDownload = 'Download the js bundles and sourcemaps for me'
+const shouldNotDownload = 'I already have the js bundles and sourcemaps'
+
+const defaultBundlesPath = 'build/static/js'
+
+coverageAndDownloadPrompts({ shouldDownload, shouldNotDownload }).then(
+  answers => {
+    const coverage = require(`./${answers.coverage}`)
+    debugger
+    if (answers.download === shouldNotDownload) {
+      inquirer
+        .prompt([
+          {
+            type: 'fuzzypath',
+            itemType: 'directory',
+            excludePath: nodePath => nodePath.startsWith('node_modules'),
+            message: 'Provide the directory path to the bundles and sourcemaps',
+            excludePath: nodePath => nodePath.startsWith('node_modules'),
+            default: fs.existsSync(defaultBundlesPath)
+              ? defaultBundlesPath
+              : undefined,
+            name: 'path'
+          }
+        ])
+        .then(answers => {
+          const urls = jsURLsFromCoverage(coverage)
+          const paths = createListOfLocalPaths({
+            files: urls,
+            path: answers.path
+          })
+          visualizeBundles({
+            bundles: paths,
+            visHTML,
+            bundleFolderName,
+            coverageFilePath: answers.coverage
+          })
+        })
+    } else {
+      initDownloadFilesFlow({
+        coverage,
+        bundleFolderName,
+        coverageFilePath: answers.coverage
+      }).then(fetchedFileNames => {
+        const paths = createListOfLocalPaths({
+          files: fetchedFileNames,
+          path: bundleFolderName
+        })
+        visualizeBundles({ bundles: paths, visHTML, bundleFolderName })
+      })
+    }
+  }
+)
