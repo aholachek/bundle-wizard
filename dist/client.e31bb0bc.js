46fb751e64db6d453dbd269e7a5cfae75a66cfac
Alex Holachek <aholachek@gmail.com>
2020-02-17 15:36:44 -0500

    improve summary styling

--- dist/client.e31bb0bc.js
+++ dist/client.e31bb0bc.js
@@ -63345,7 +63345,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56273" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57711" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
